6f8cf188e9670a796e21cda7a2bab6272eb3bb49
sgujuluva <sanghee04@gmail.com>
2022-09-10 20:55:59 +0200

    styling products

--- src/components/Context/Context.jsx
+++ src/components/Context/Context.jsx
@@ -5,14 +5,12 @@ import { products } from "../data.js";
 //creatingContext
 export const ProdContext = createContext();
 
-console.log("the prod in context are:",products)
+console.log("the prod in context are:", products);
 
 function Context({ children }) {
   return (
-<ProdContext.Provider value = {{products}}>
-    {children}
-    </ProdContext.Provider>
-  ) 
+    <ProdContext.Provider value={{ products }}>{children}</ProdContext.Provider>
+  );
 }
 
 export default Context;
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,31 +1,32 @@
-import React, {useContext} from 'react';
-import {Link} from "react-router-dom"
-import {ProdContext} from "../Context/Context";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { ProdContext } from "../Context/Context";
 //styles
-import "./Products.css"
+import "./Products.css";
 
 function Products() {
-
-    const {products} = useContext(ProdContext);
-    console.log("products in prod component",products)
+  const { products } = useContext(ProdContext);
+  console.log("products in prod component", products);
 
   return (
-    <div className='products'>
-    {products?.map(item => (
+    <div className="products">
+      {products?.map((item) => (
         <div className="card" key={item.id}>
-            <Link to={`/product/${item.id}`}><img src={item.src} alt="" /></Link>
-            <div className="content">
-                <h3>
-                <Link to={`/product/${item.id}`}>{item.title}</Link>
-                </h3>
-                <span>{item.price.toFixed(2)} €</span>
-               <p>{item.description}</p>
-               <button>Add to Cart</button>
-            </div>
+          <Link to={`/product/${item.id}`}>
+            <img src={item.src} alt="" />
+          </Link>
+          <div className="content">
+            <h3>
+              <Link to={`/product/${item.id}`}>{item.title}</Link>
+            </h3>
+            <span>{item.price.toFixed(2)} €</span>
+            <p>{item.description}</p>
+            <button>Add to Cart</button>
+          </div>
         </div>
-    ))}
+      ))}
     </div>
-  )
+  );
 }
 
-export default Products
+export default Products;
