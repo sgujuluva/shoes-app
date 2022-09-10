df7e3ec99a3ad1b7b7861a7e7ccd45fd03562505
sgujuluva <sanghee04@gmail.com>
2022-09-10 16:15:16 +0200

    data file created

---
+++ src/components/data.js
@@ -0,0 +1,56 @@
+export const products = [
+    {
+        "id": "1",
+        "title": "Nike Shoes 01",
+        "src": "https://cdn.pixabay.com/photo/2014/04/05/11/38/nike-316449__340.jpg",
+        "description": "Sneakers running shoe",
+        "price": 83,
+        "colors":["red","black","crimson","teal"],
+        "count": 1
+    },
+    {
+        "id": "2",
+        "title": "Nike Shoes 02",
+        "src": "https://cdn.pixabay.com/photo/2020/07/15/18/32/sneakers-5408674__340.png",
+        "description": "Sneakers shoe jeans",
+        "price": 95,
+        "colors":["red","crimson","teal"],
+        "count": 1
+    },
+    {
+        "id": "3",
+        "title": "Nike Shoes 03",
+        "src": "https://cdn.pixabay.com/photo/2020/07/15/18/30/sneakers-5408667__340.png",
+        "description": "Sneakers shoe fashion",
+        "price": 90,
+        "colors":["lightblue","white","crimson","teal"],
+        "count": 1
+    },
+    {
+        "id": "4",
+        "title": "Nike Shoes 04",
+        "src": "https://cdn.pixabay.com/photo/2018/11/27/07/03/products-3840888__340.jpg",
+        "description": "Nike Product Shoe",
+        "price": 55,
+        "colors":["orange","black","crimson","teal"],
+        "count": 1
+    },
+    {
+        "id": "5",
+        "title": "Nike Shoes 05",
+        "src": "https://cdn.pixabay.com/photo/2021/02/11/17/51/jordan-6005991__340.jpg",
+        "description": "Jordan jordan Nike",
+        "price": 87,
+        "colors":["orange","black","crimson","teal"],
+        "count": 1
+    },
+    {
+        "id": "6",
+        "title": "Nike Shoes 06",
+        "src": "https://cdn.pixabay.com/photo/2020/12/03/10/33/shoe-5800164__340.jpg",
+        "description": "Shoe Nike Fashion",
+        "price": 67,
+        "colors":["orange","black","crimson","teal"],
+        "count": 1
+    }
+]
