2690fcb9c17fea5f61c06b2899e3edeb3c052fd9
sgujuluva <sanghee04@gmail.com>
2022-09-10 20:42:26 +0200

    prod listed on products componnt

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, {useContext} from 'react';
+import {Link} from "react-router-dom"
+import {ProdContext} from "../Context/Context";
 //styles
 import "./Products.css"
 
 function Products() {
+
+    const {products} = useContext(ProdContext);
+    console.log("products in prod component",products)
+
   return (
-    <div>Products</div>
+    <div className='products'>
+    {products?.map(item => (
+        <div className="card" key={item.id}>
+            <Link to={`/product/${item.id}`}><img src={item.src} alt="" /></Link>
+            <div className="content">
+                <h3>
+                <Link to={`/product/${item.id}`}>{item.title}</Link>
+                </h3>
+                <span>{item.price.toFixed(2)} €</span>
+               <p>{item.description}</p>
+               <button>Add to Cart</button>
+            </div>
+        </div>
+    ))}
+    </div>
   )
 }
 
