0cd5f69aa485a992abbb3f5d700e1fc6ff0a6896
sgujuluva <sanghee04@gmail.com>
2022-09-10 19:10:02 +0200

    header styled but mobile responsive not

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,41 +1,75 @@
-import React from 'react'
-import {Link} from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 //images
 import MenuIcon from "../../images/menuicon.png";
 import CloseIcon from "../../images/closeicon.png";
 import CartIcon from "../../images/carticon.png";
 import Nikelogo from "../../images/nikelogo.png";
-
-
+//styles
+import "./Header.css";
 
 function Header() {
+  //toggle menu and close button
+  const [toggle, setToggle] = useState(false);
+
+  const toggleHandle = () => {
+    setToggle( !toggle);
+  };
+  console.log("togle is:", toggle);
+
   return (
-    <div className = "header">
-        <div className="header-menu">
-    <img src={MenuIcon} alt="menu icon" width="20"/>
-    <div className="logo">
-        <h1><Link to="/"><img src={Nikelogo} width="30" alt="" />Nike</Link></h1>
-    </div>
-    <nav>
-        <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to ="/product">Product</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-            <li><Link to="/login">Login</Link></li>
-            <li>
-                <img src={CloseIcon} alt="close" width="20" />
-            </li>
-        </ul> 
-        <div className="nav-cart">
-            <Link to="/cart"> <img src={CartIcon} alt="cart icon" width="25"/>
+    <div className="header">
+      <div className="header-menu">
+        <img
+          onClick={toggleHandle}
+          className="menu"
+          src={MenuIcon}
+          alt="menu icon"
+          height="20"
+        />
+        <div className="logo">
+          <h1>
+            <Link to="/">
+              <img src={Nikelogo} width="30" alt="" />
+              Nike
             </Link>
-           
+          </h1>
+        </div>
+        <nav>
+          <ul className={toggle ? "toggle" : ""}>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/product">Product</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <img
+                onClick={toggleHandle}
+                className="close"
+                src={CloseIcon}
+                alt="close"
+                width="15"
+              />
+            </li>
+          </ul>
+          <div className="nav-cart">
+            <Link to="/cart">
+              <img src={CartIcon} alt="cart icon" width="25" />
+            </Link>
+
             <span>0</span>
-        </div>
-    </nav>
-        </div>
+          </div>
+        </nav>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
