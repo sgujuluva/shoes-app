256a59077702474f217588cbac38819c32765960
sgujuluva <sanghee04@gmail.com>
2022-09-10 16:37:34 +0200

    added logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import {Link} from "react-router-dom";
+//images
+import MenuIcon from "../../images/menuicon.png";
+import CloseIcon from "../../images/closeicon.png";
+import CartIcon from "../../images/carticon.png";
+import Nikelogo from "../../images/nikelogo.png";
+
+
 
 function Header() {
   return (
     <div className = "header">
         <div className="header-menu">
-            
+    <img src={MenuIcon} alt="menu icon" width="20"/>
+    <div className="logo">
+        <h1><Link to="/"><img src={Nikelogo} width="30" alt="" />Nike</Link></h1>
+    </div>
         </div>
     </div>
   )
