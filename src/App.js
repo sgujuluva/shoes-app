f0c550adae025adb9de605339e262bd1408fbb95
sgujuluva <sanghee04@gmail.com>
2022-09-11 21:47:24 +0200

    home page designed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import {Routes, Route} from "react-router-dom";
 //components
 import Header from "./components/Header/Header"
 import Display from "./components/Display/Display";
-
+//img
+import HomeShoe from "./images/home-shoe.png";
+import ShoeCollection from "./images/shoe-collection.png";
 //styles
 import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
@@ -13,6 +15,11 @@ function App() {
     <div className="App">
       <Header/>
       <Display/>
+      <div className="home-container">
+            <img src={HomeShoe} alt="" />
+            <img src={ShoeCollection} alt="" />
+      </div>
+      
     </div>
   );
 }
