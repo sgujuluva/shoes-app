a7019209ae18575c1f84a7808af69cd1c07f34ef
sgujuluva <sanghee04@gmail.com>
2022-09-11 22:14:39 +0200

    payment component added

--- src/components/Display/Display.jsx
+++ src/components/Display/Display.jsx
@@ -4,6 +4,7 @@ import Products from "../Products/Products";
 import { Routes, Route } from "react-router-dom";
 import Cart from "../Cart/Cart";
 import Home from "../Home/Home";
+import Payment from "../Payment/Payment";
 
 function Display() {
   return (
@@ -13,6 +14,7 @@ function Display() {
         <Route path="/product" element={<Products />} />
         <Route path="/product/:id" element={<SingleProd />} />
         <Route path="/cart" element = {<Cart/>}/>
+        <Route path="/payment" element = {<Payment/>}/>
       </Routes>
     </div>
   );
---
+++ src/components/Payment/Payment.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Payment() {
+  return (
+    <div>
+        Payment</div>
+  )
+}
+
+export default Payment
