fb30975fc1dfc70d0694636a7a4898bfa012db12
sgujuluva <sanghee04@gmail.com>
2022-09-11 15:03:19 +0200

    used params to finfthe prod

--- src/components/SingleProd/SingleProd.jsx
+++ src/components/SingleProd/SingleProd.jsx
@@ -1,10 +1,18 @@
-import React, {useContext} from 'react'
-import { ProdContext } from "../Context/Context";
+import React, {useContext} from 'react';
+import {useLocation, useParams} from "react-router-dom"; //hook to  get current pathname 
+//import { ProdContext } from "../Context/Context";
 import "./SingleProd.css";
 
 function SingleProd() {
-  const { products } = useContext(ProdContext);
- console.log(this.props)
+
+  //const { products } = useContext(ProdContext);
+
+ /*  const location = useLocation(); //to get current url
+  console.log("location is",location) */
+  const params = useParams();
+  console.log("params are:", params.id)
+ 
+
   return (
     <div>SingleProd</div>
   )
