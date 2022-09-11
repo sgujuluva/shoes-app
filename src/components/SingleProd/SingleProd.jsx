845b21483c6cbf63457895a806dfea33963ebe59
sgujuluva <sanghee04@gmail.com>
2022-09-11 15:36:33 +0200

    single prd done

--- src/components/SingleProd/SingleProd.jsx
+++ src/components/SingleProd/SingleProd.jsx
@@ -1,21 +1,34 @@
-import React, {useContext} from 'react';
-import {useLocation, useParams} from "react-router-dom"; //hook to  get current pathname 
-//import { ProdContext } from "../Context/Context";
+import React, { useContext } from "react";
+import { Link, useLocation, useParams } from "react-router-dom"; //hook to  get current pathname
+import { ProdContext } from "../Context/Context";
 import "./SingleProd.css";
 
 function SingleProd() {
+  const { products } = useContext(ProdContext);
 
-  //const { products } = useContext(ProdContext);
-
- /*  const location = useLocation(); //to get current url
-  console.log("location is",location) */
-  const params = useParams();
-  console.log("params are:", params.id)
- 
+  const params = useParams(); // to get the particular prod id with params
+  console.log("params are:", params.id);
+  //getting that particular prod using filter
+  let filterProd = products.filter((item) => item.id === params.id);
+  console.log("filteredProd is", filterProd);
 
   return (
-    <div>SingleProd</div>
-  )
+    <>
+      {filterProd.map((item) => (
+        <div className="single-prod-details">
+          <img src={item.src} alt="" />
+          <div className="box">
+            <div className="row">
+              <h3>{item.title}</h3>
+              <h2>{item.price.toFixed(2)} €</h2>
+            </div>
+            <p>{item.description}</p>
+            <Link to="/cart">Add to Cart</Link>
+          </div>
+        </div>
+      ))}
+    </>
+  );
 }
 
-export default SingleProd
+export default SingleProd;
