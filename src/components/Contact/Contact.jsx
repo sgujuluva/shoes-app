8e775889da17baee4af44ae751b5ee1dd25d6f98
sgujuluva <sanghee04@gmail.com>
2022-09-21 21:05:25 +0200

    contact form styled

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,18 +4,26 @@ import "./contact.css";
 function Contact() {
   const handleSubmit = (e) => {
     e.preventDefault();
+
   }
   return (
     <div className="contact-details">
+      <h3>Contact Form</h3>
+      <div className="contact-container">
       <form onSubmit={handleSubmit}>
-            <input type="text" name="user_name" placeholder="Name" />
-            <input type="text" name="user_subject" placeholder="Subject" />
-            <input type="email" name="user_email" placeholder="Email" />
-            <textarea rows="7" cols="20" name="comment">
-              Enter your message here...
+        
+            <input type="text" required name="user_name" placeholder="Name" />
+           
+            <input type="text" required  name="user_subject" placeholder="Subject" />
+          
+            <input type="email" required name="user_email" placeholder="Email" />
+           
+            <textarea rows="7" placeholder="Enter your message here..."  cols="20" name="comment">
+             
             </textarea>
             <button>Submit</button>
           </form>
+          </div>
         </div>
   )
 }
