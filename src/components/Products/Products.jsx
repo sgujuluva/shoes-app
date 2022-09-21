76b1e66d8fb7ac9a2876d2a01c7eea97f71d85b4
sgujuluva <sanghee04@gmail.com>
2022-09-21 14:33:45 +0200

    products card styled

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,28 +1,27 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { ProdContext } from "../Context/Context";
-import {useCart} from "react-use-cart";
+import { useCart } from "react-use-cart";
 //styles
 import "./Products.css";
 import Cart from "../Cart/Cart";
 
-
 function Products() {
-
   const { products } = useContext(ProdContext);
   console.log("products in prod component", products);
 
-//usecart
-const {addItem} = useCart();
+  //usecart
+  const { addItem } = useCart();
 
   return (
     <div className="products">
-      {products?.map((item) => (
-        <div className="card" key={item.id}>
-          <Link to={`/product/${item.id}`}>
-            <img src={item.src} alt="" />
-          </Link>
-          <div className="content">
+      <div className="products-container">
+        {products?.map((item) => (
+          <div className="card" key={item.id}>
+            <Link to={`/product/${item.id}`}>
+              <img src={item.src} alt="" />
+            </Link>
+            {/* <div className="content"> */}
             <h3>
               <Link to={`/product/${item.id}`}>{item.title}</Link>
             </h3>
@@ -30,9 +29,10 @@ const {addItem} = useCart();
             <p>{item.description}</p>
             <button onClick={() => addItem(item)}>Add to Cart</button>
           </div>
-        
-        </div>
-      ))}
+
+          /*    </div> */
+        ))}
+      </div>
     </div>
   );
 }
