9c770774ead0aaf3264e8bdc7f00e1b142108e8c
sgujuluva <sanghee04@gmail.com>
2022-09-23 17:53:17 +0200

    product button linked to single product

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -19,15 +19,17 @@ function Products() {
         {products?.map((item) => (
           <div className="card" key={item.id}>
             <Link to={`/product/${item.id}`}>
-              <img src={item.src} alt="" />
-            </Link>
-            {/* <div className="content"> */}
-            <h3>
+            <img src={item.src} alt="" />
+            </Link> 
+            {/*  <img src={item.src} alt="" /> */}          
+           {/*  <h3>
               <Link to={`/product/${item.id}`}>{item.title}</Link>
-            </h3>
+            </h3> */}
+            <h3>{item.title}</h3>
             <span>{item.price.toFixed(2)} €</span>
             <p>{item.description}</p>
-            <button onClick={() => addItem(item)}>Add to Cart</button>
+            {/* <button onClick={() => addItem(item)}>Add to Cart</button> */}
+            <Link to={`/product/${item.id}`}><button>Explore</button></Link> 
           </div>
 
           /*    </div> */
